0197b704e1d7bb6e7267437036ddca97ae5dcff7
katya.talluri <katya.talluri@workday.com>
2022-03-23 17:35:20 -0700

    more commits

--- 3WhileControlLoop/PrintMaxAndMin.py
+++ 3WhileControlLoop/PrintMaxAndMin.py
@@ -1,1 +1,21 @@
 #Prompt the user to enter numbers and  use "!" to terminate. Print the max and min values of the numbers printed by the user
+
+print('Enter numbers  and type ! when  you are finished.')
+inp = input()
+num = 0
+if inp != '!':
+    max = int(inp)
+    min = int(inp)
+else:
+    exit()
+while(inp != '!'):
+    num = int(inp)
+    if num > max:
+        max = int(inp)
+    if num < min:
+        min = int(inp)
+    print('Enter numbers  and type ! when  you are finished.')
+    inp = input()
+
+print('Max: %d' % (max))
+print('Min: %d' % (min))
